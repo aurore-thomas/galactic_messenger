f8623ad6b6c67cc93eea5479ec7e8eb8f616c277
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2023-10-18 15:05:33 +0200

    [ADD] functional server authentication

--- server/src/main/java/main/model/User.java
+++ server/src/main/java/main/model/User.java
@@ -20,6 +20,6 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    private String salt;
+//    @Column(nullable = false)
+//    private String salt;
 }
