3116f2d443dab6d85999fdfad7fe9127d191036c
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2023-10-18 12:02:15 +0200

    [ADD] port as arg

--- server/src/main/java/main/ServerApp.java
+++ server/src/main/java/main/ServerApp.java
@@ -11,10 +11,16 @@ public class ServerApp {
 
     public static void main(String[] args) throws UnknownHostException {
 
+        if (args.length > 0) {
+            System.setProperty("server.port", String.valueOf(Integer.parseInt(args[0])));
+        }
+
+        int port = Integer.parseInt(System.getProperty("server.port"));
+
         SpringApplication.run(ServerApp.class, args);
         InetAddress inetAddress = InetAddress.getLocalHost();
         String ipAddress = inetAddress.getHostAddress();
-        System.out.println("Server available at:" + ipAddress + ":3000");
-        System.out.println("H2 Console access : http://localhost:3000/h2");
+        System.out.println("\nServer available at: " + ipAddress + ":" + port);
+        System.out.println("H2 Console access : http://localhost:" + port + "/h2");
     }
 }
